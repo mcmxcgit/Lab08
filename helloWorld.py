30249d9adaab8c786fb47446b94dbbfb298f24f3
mcmxcgit <mcmxcgit@github.com>
2023-03-29 00:24:32 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,7 @@
-userName = input("Hello! Please enter your name!")
-print("Hello", userName, "nice to meet you")
+userName = input("Hello! Please enter your facebook username!")
+userPassword = input("Hello! Please enter your facebook password!")
+
+if(userName.lower()=="michael" and userPassword=="DeepSpace9Babylon5"):
+	print("Welcome to facebook!")
+else:
+	print("Wrong credentials, try again!")
